02ec24166c7223947c6886dc82cf30b7cc4ed8d5
赵鑫 <zhaoxin@hualala.com>
2022-05-11 16:47:44 +0800

    feat: resolvePromise实现

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -128,5 +128,75 @@ class MPromise {
     isFunction(param) {
         return typeof param === 'function'
     }
+
+    resolvePromise(newPromise, x, resolve, reject) {
+        // 如果 newPromise 和 x 指向同一对象，以 TypeError 为据因拒绝执行 newPromise
+        // 这是为了防止死循环
+        if (newPromise === x) {
+            throw reject(new TypeError('The promise and the return value are the same'))
+        }
+
+        if (x instanceof MPromise) {
+            // 如果 x 为 Promise ，则使 newPromise 接受 x 的状态
+            // 也就是继续执行x，如果执行的时候拿到一个y，还要继续解析y
+            x.then((y) => {
+                this.resolvePromise(newPromise, y, resolve, reject)
+            }, reject);
+        } else if (typeof x === 'object' || this.isFunction(x)) {
+            // 如果 x 为对象或者函数
+            if (x === null) {
+                // null也会被判断为对象
+                return resolve(x)
+            }
+
+            let then = null;
+
+            try {
+                // 把 x.then 赋值给 then 
+                then = x.then
+            } catch (error) {
+                // 如果取 x.then 的值时抛出错误 e ，则以 e 为据因拒绝 promise
+                return reject(error)
+            }
+
+            // 如果 then 是函数
+            if (isFunction(then)) {
+                let called = false;
+                // 将 x 作为函数的作用域 this 调用
+                // 传递两个回调函数作为参数，第一个参数叫做 resolvePromise ，第二个参数叫做 rejectPromise
+                try {
+                    then.call(
+                        x,
+                        // 如果 resolvePromise 以值 y 为参数被调用，则运行 resolvePromise
+                        (y) => {
+                            // 需要有一个变量called来保证只调用一次.
+                            if (called) return;
+                            called = true;
+                            this.resolvePromise(newPromise, y, resolve, reject)
+                        },
+                        // 如果 rejectPromise 以据因 r 为参数被调用，则以据因 r 拒绝 promise
+                        (r) => {
+                            if (called) return;
+                            called = true;
+                            reject(r);
+                        }
+                    )
+                } catch (error) {
+                    // 如果调用 then 方法抛出了异常 e
+                    if (called) return;
+
+                    // 否则以 e 为据因拒绝 promise
+                    reject(error)
+                }
+            } else {
+                // 如果 then 不是函数，以 x 为参数执行 promise
+                resolve(x)
+            }
+
+        } else {
+            // 如果 x 不为对象或者函数，以 x 为参数执行 promise
+            resolve(x);
+        }
+    }
 }
 
