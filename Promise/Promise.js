888c2ded1422bac13cc7ceaa4d50d1ffb5bd6b8a
赵鑫 <zhaoxin@hualala.com>
2022-05-11 15:51:36 +0800

    feat: then方法pendind值处理

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -19,6 +19,34 @@ class MPromise {
         }
     }
 
+    // 6.3 定义数据，存储数据
+    FULFILLED_CALLBACK_LIST = [];
+    REJECTED_CALLBACK_LIST = [];
+    _status = PENDING;
+
+    // 6.4 status变化处理
+    get status() {
+        return this._status;
+    }
+
+    set status(newStatus) {
+        switch (newStatus) {
+            case FULFILLED: {
+                this.FULFILLED_CALLBACK_LIST.forEach(callback => {
+                    callback(this.value)
+                })
+                break
+            }
+
+            case REJECTED: {
+                this.REJECTED_CALLBACK_LIST.forEach(callback => {
+                    callback(this.reason)
+                })
+                break;
+            }
+        }
+    }
+
     // 第四步：实现 resolve 和 reject 方法
     resolve(value) {
         if (this.state === PENDING) {
@@ -44,7 +72,7 @@ class MPromise {
             throw reason;
         }
 
-        // 根据当前的 Promise 状态，调用不同的函数
+        // 6.2 根据当前的 Promise 状态，调用不同的函数
         switch (this.state) {
             case FULFILLED: {
                 fulFilledFn(this.value);
@@ -55,10 +83,16 @@ class MPromise {
                 rejectedFn(this.reason);
                 break;
             }
+
+            case PENDING: {
+                this.FULFILLED_CALLBACK_LIST.push(realOnFulfilled);
+                this.REJECTED_CALLBACK_LIST.push(realOnRejected);
+                break;
+            }
         }
     }
 
-    // 检查并处理参数，之前提到的如果不是 function, 就忽略. 这个忽略指的是原样返回 value 或 reason
+    // 6.1 检查并处理参数，之前提到的如果不是 function, 就忽略. 这个忽略指的是原样返回 value 或 reason
     isFunction(param) {
         return typeof param === 'function'
     }
